e195ee707af4f323d6fc72ae089a71172c6244db
MEGAMINDSPARK <rahulkumarroy399@gmail.com>
2023-09-12 14:18:55 +0530

    conditional

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,29 +1,45 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css';
-import React,{useState} from 'react';
-import ExpenseFilter from './ExpenseFilter';
 
-const Expenses = (props) =>{
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  const [filteredYear,setFilteredYear] = useState('2020');
-
-  const filterChangeHandler = selectedYear => {
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
+  };
+
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
+  let expensesContent = <p>No expenses found.</p>;
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
   }
 
   return (
-    <Card className="expenses">
-    <ExpenseFilter selected={filteredYear} onChangeFilter = {filterChangeHandler} />
-      {props.itemhai.map(
-        expense => <ExpenseItem
-        date={expense.date}
-        title={expense.title}
-        amount={expense.amount}
+    <div>
+      <Card className='expenses'>
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
         />
-      )}
-    </Card>
+        {expensesContent}
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
